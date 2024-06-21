1dabc7558761a8eaa0c4cedfb3df4c1848018b17
alirezaseif20 <alirezaariye@gmail.com>
2024-06-21 18:24:11 +0330

    feat: add BannerSlider

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
+import 'package:ecommerce/constants/colors.dart';
+import 'package:ecommerce/widgets/banner_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key}); 
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+          body: SafeArea(
+        child: BannerSlider(),
+      )),
     );
   }
 }
+
