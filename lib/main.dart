93990e71db31e1a22842475f91adfd187e6cf7f9
alirezaseif20 <alirezaariye@gmail.com>
2024-06-23 20:05:07 +0330

    feat: add CategoryItemList widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:ecommerce/constants/colors.dart';
 import 'package:ecommerce/widgets/banner_slider.dart';
+import 'package:ecommerce/widgets/category_item_list.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -13,11 +15,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-          body: SafeArea(
-        child: BannerSlider(),
-      )),
+        backgroundColor: CustomColors.backgroundScreenColor,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemExtent: 75,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return const CategoryHorizontalteList();
+              },
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/category_item_list.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class CategoryHorizontalteList extends StatelessWidget {
+  const CategoryHorizontalteList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              height: 56,
+              width: 56,
+              decoration: ShapeDecoration(
+                color: Colors.blue,
+                shadows: const [
+                  BoxShadow(
+                      color: Colors.blue,
+                      blurRadius: 20,
+                      spreadRadius: -10,
+                      offset: Offset(0, 10))
+                ],
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                ),
+              ),
+            ),
+            const Icon(
+              Icons.mouse,
+              color: Colors.white,
+              size: 32,
+            )
+          ],
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          'همه',
+          style: TextStyle(fontFamily: 'SB', fontSize: 12),
+        )
+      ],
+    );
+  }
+}
