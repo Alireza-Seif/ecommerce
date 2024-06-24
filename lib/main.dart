a00a3eb7430b30dbbb98c04f1e22e0ed3f0354c9
alirezaseif20 <alirezaariye@gmail.com>
2024-06-24 22:23:27 +0330

    feat: add ProductItem

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce/constants/colors.dart';
-import 'package:ecommerce/widgets/category_item_list.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +15,157 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         backgroundColor: CustomColors.backgroundScreenColor,
-        body: SafeArea(child: const CategoryHorizontaltemList()),
+        body: SafeArea(
+          child: Center(
+            child: const ProductItem(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 216,
+      width: 160,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const SizedBox(height: 10),
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Expanded(child: Container()),
+              Image.asset('assets/images/iphone.png'),
+              Positioned(
+                top: 0,
+                right: 10,
+                child: SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: Image.asset(
+                    'assets/images/active_fav_product.png',
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 5,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(
+                        15,
+                      ),
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: 3, horizontal: 6),
+                    child: Text(
+                      '%4',
+                      style: TextStyle(
+                          fontFamily: 'SB',
+                          fontSize: 12,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Spacer(),
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                'آیفون 13 پرومکس',
+                style: TextStyle(
+                  fontFamily: 'SM',
+                  fontSize: 14,
+                ),
+              ),
+              SizedBox(height: 10)
+            ],
+          ),
+          Container(
+            height: 53,
+            decoration: const BoxDecoration(
+              color: CustomColors.blue,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(15),
+                bottomRight: Radius.circular(15),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: CustomColors.blue,
+                  blurRadius: 25,
+                  spreadRadius: -12,
+                  offset: Offset(0, 15),
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  const Text('تومان',
+                      style: TextStyle(
+                        fontFamily: 'SM',
+                        fontSize: 12,
+                        color: Colors.white,
+                      )),
+                  const SizedBox(width: 5),
+                  const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '49.800.000',
+                        style: TextStyle(
+                          fontFamily: 'SM',
+                          fontSize: 12,
+                          color: Colors.white,
+                          decoration: TextDecoration.lineThrough,
+                        ),
+                      ),
+                      Text(
+                        '47.800.000',
+                        style: TextStyle(
+                          fontFamily: 'SM',
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    width: 24,
+                    child: Image.asset(
+                        'assets/images/icon_right_arrow_cricle.png'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
