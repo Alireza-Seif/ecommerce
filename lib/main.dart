6e3e847741fbb50125046e4afb7389d569257e3d
alirezaseif20 <alirezaariye@gmail.com>
2024-06-25 16:40:49 +0330

    feat: add BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:ecommerce/constants/colors.dart';
 import 'package:ecommerce/screens/product_list_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -10,9 +13,44 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ProductListScreen(),
+    return MaterialApp(
+      home: Scaffold(
+        body: const ProductListScreen(),
+        bottomNavigationBar: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 40, sigmaY: 40),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Container(
+                        decoration: const BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: CustomColors.blue,
+                                spreadRadius: -7,
+                                blurRadius: 20,
+                                offset: Offset(0, 13)),
+                          ],
+                        ),
+                        child: Image.asset('assets/images/icon_home.png')),
+                    label: 'home'),
+                BottomNavigationBarItem(
+                    icon: Image.asset('assets/images/icon_home.png'),
+                    label: 'a'),
+                BottomNavigationBarItem(
+                    icon: Image.asset('assets/images/icon_home.png'),
+                    label: 'b'),
+                BottomNavigationBarItem(
+                    icon: Image.asset('assets/images/icon_home.png'),
+                    label: 'c'),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
